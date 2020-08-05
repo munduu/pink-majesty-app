6a6f6fc8d7b5a4b84915e22bac5de4c5aaae780d
danielmcstr <danielcastro.97@gmail.com>
2020-08-05 16:52:53 -0300

    Update webhook.php

--- php/stripe/webhook.php
+++ php/stripe/webhook.php
@@ -1,14 +1,81 @@
 <?php
-$endpoint_secret = 'whsec_...';
+require_once('../Connections/localhost.php');
+require_once('../function/log.php');
+require_once('../function/function.php');
+require_once('config.php');
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+//error_reporting(E_ALL ^ E_NOTICE);
+
+$endpoint_secret = 'whsec_anh594dRZVTC2dIOhydwHtyR9IKvBJYs';
 
 $payload = @file_get_contents('php://input');
 $sig_header = $_SERVER['HTTP_STRIPE_SIGNATURE'];
-$event = null;
+$json = json_decode($payload);
+salva_log('webhook',$json);
+$log = json_encode($json,JSON_PRETTY_PRINT);
+echo $json->object;
+echo "\n";
+if(!empty($json->object)){
+    if($json->object == 'event'){
+        echo $type = $json->type;
+        echo "\n";
+        if ($type == "payment_intent.succeeded") {
+            $charge = $json->data->object->charges->data[0];
+            $payment_intent = $json->data->object->id;
+            $amount = $charge->amount;
+            $customer = $charge->customer;
+            $type = $status = ($charge->paid == true) ? 'paid' : $charge->status;
+            http_response_code(200);
+        } else {
+            $object = $json->data->object;
+            $customer = $object->customer;
+            $amount = $object->amount;
+            $status = $object->status;
+            http_response_code(200);
+        }
+        $amount = $amount/100.0;
+    } else {
+        echo "Not Event";
+        echo "\n";
+        http_response_code(400);
+    }
+} else {
+    echo "Empty";
+    echo "\n";
+    http_response_code(400);
+}
 
-try {
+$sql = "SELECT * FROM tb_login WHERE gateway_id = '$customer' ORDER BY id";
+    $resultado 	= mysql_query($sql) or die(mysql_error());
+    if (mysql_num_rows($resultado) > 0){
+        while($ln = mysql_fetch_assoc($resultado)){
+            $id_cliente = $ln['id_cliente'];
+        }
+    }
+    
+$sql = "INSERT INTO `tb_webhook`(`cliente_id`, `customer`,`payment_intent`, `valor`, `status`, `log`) VALUES ('$id_cliente','$customer','$payment_intent','$amount', '$type','$log')";
+        $resultado 	= mysql_query($sql) or die(mysql_error());
+        http_response_code(200);
+
+/*try {
     $event = \Stripe\Webhook::constructEvent(
         $payload, $sig_header, $endpoint_secret
     );
+    if ($event->type == "charge.succeeded") {
+        $charge = $event->data->object;
+        $amount = $charge->amount;
+        $customer = $charge->customer;
+        $status = ($charge->paid)? 'paid':$charge->status;
+        http_response_code(200);
+        exit();
+    } else {
+        $charge = $event->data->object;
+        $amount = $charge->amount;
+        http_response_code(200);
+    }
+    echo $sql = "INSERT INTO `tb_webhook`(`valor`, `status`, `log`) VALUES ('$amount', '$status','$log')";
+    $resultado 	= mysql_query($sql) or die(mysql_error());
 } catch(\UnexpectedValueException $e) {
     // Invalid payload
     http_response_code(400);
@@ -18,16 +85,4 @@ try {
     http_response_code(400);
     exit();
 }
-
-if ($event->type == "payment_intent.succeeded") {
-    $intent = $event->data->object;
-    printf("Succeeded: %s", $intent->id);
-    http_response_code(200);
-    exit();
-} elseif ($event->type == "payment_intent.payment_failed") {
-    $intent = $event->data->object;
-    $error_message = $intent->last_payment_error ? $intent->last_payment_error->message : "";
-    printf("Failed: %s, %s", $intent->id, $error_message);
-    http_response_code(200);
-    exit();
-}
+?>
