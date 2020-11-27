e2ddd13540068e5685bc7799d66b6d5eff1292d0
danielmcstr <danielcastro.97@gmail.com>
2020-11-27 08:51:47 -0300

    Update config.php

--- php/stripe/config.php
+++ php/stripe/config.php
@@ -1,13 +1,21 @@
 <?php
-ini_set('display_errors', true);
-ini_set('display_startup_errors', true);
-error_reporting(E_ALL);
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(0);
 include "vendor/autoload.php";
 header("Access-Control-Allow-Origin: *");
 header("Content-type: application/json");
 ob_start();
 //GLOBAL API KEY
-define('STRIPE_API_KEY','sk_test_2AItDw6QvoZr2UWxAlntETu500I2Mdm19B');
+$env = 'sandbox';
+if($env == 'production'){
+    define('STRIPE_API_KEY','sk_live_KHsN5AyaPggsMq1WT5VTgop200bPMwO6pp');
+    define('STRIPE_TOKEN','pk_live_IPxD1R42AJYMtlg68dqRh8kn00AgrABsGZ');
+} else {
+    define('STRIPE_API_KEY','sk_test_2AItDw6QvoZr2UWxAlntETu500I2Mdm19B');
+    define('STRIPE_TOKEN','pk_test_HwOzl6pIove5P3TZopMpaOsg001qQzh3P6');
+}
+
 $stripe = \Stripe\Stripe::setApiKey(STRIPE_API_KEY);
 //$stripe = \Stripe\Stripe::setApiKey("rk_test_98I5zg9voEEYXMN75Uh18qNG003DQGrIkR");
 function salva_log($path,$log){
